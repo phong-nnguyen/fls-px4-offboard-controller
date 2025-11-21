79e80b76ebb23491dacb055f09d6772706e1b2b6
phong-nnguyen <phongn102004@gmail.com>
2025-11-20 17:51:42 -0800

    debug

--- controller.py
+++ controller.py
@@ -1362,7 +1362,7 @@ if __name__ == "__main__":
     set_point_thread = Thread(target=c.test_set_point)
     set_point_thread.start()
 
-    if not c.set_mode('Offboard'):
+    if not c.set_mode('OFFBOARD'):
         pass
         # exit()
 
