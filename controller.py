44a632214e3c6341bcbd3032bb3a43741654989f
phong-nnguyen <phongn102004@gmail.com>
2025-12-02 02:52:53 -0800

    force preflight checks

--- controller.py
+++ controller.py
@@ -244,6 +244,9 @@ class Controller:
                 1, 21196, 0, 0, 0, 0, 0
             )
 
+            msg = self.master.recv.match(type="COMMAND_ACK", blocking=True)
+            self.logger.info(msg)
+
             # Wait for armed status
             start = time.time()
             while time.time() - start < 2:
